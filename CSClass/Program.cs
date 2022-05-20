1816653a0087f4ab657f288af4535a53d9859761
kimdahuin12 <hin0258@naver.com>
2022-05-20 09:54:01 +0900

    #2 Math 클래스 사용해보기

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -21,13 +21,20 @@ namespace CSClass
             Console.WriteLine(random.NextDouble()*10);
 
             List<int> list = new List<int>() { 52, 273, 32, 64};
-
             list.Remove(52);
-
             foreach (int item in list)
             {
                 Console.WriteLine("count : " + list.Count+" Item : " +item);
             }
+
+            Console.WriteLine(Math.Abs(-3));
+            Console.WriteLine(Math.Max(342, 2));
+            Console.WriteLine(Math.Min(342, 2));
+            Console.WriteLine(Math.Ceiling(234.2332));
+            Console.WriteLine(Math.Floor(342.12));
+            Console.WriteLine(Math.Round(342.52));
+            Console.WriteLine(Math.Round(342.22));
+            Console.WriteLine(Math.PI);
 
         }
     }
