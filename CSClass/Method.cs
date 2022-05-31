0ac256b2cb11b1bf031c5da4071819e483636550
dahuin <s2001@e-mirim.hs.kr>
2022-05-31 16:01:00 +0900

    #17 매개변수와 반환

--- CSClass/Method.cs
+++ CSClass/Method.cs
@@ -10,5 +10,37 @@ namespace CSClass
         {
             return v1 * v2;
         }
+
+        /// <summary>
+        /// min부터 max까지의 합을 구합니다.
+        /// </summary>
+        /// <param name="min">시작값</param>
+        /// <param name="max">최대값</param>
+        /// <returns>min부터 max까지의 합계</returns>
+        public int Sum(int min, int max)
+        {
+            int res = 0;
+            for(int i = min; i <= max; i++)
+            {
+                res += i;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// min부터 max까지의 곱을 구합니다.
+        /// </summary>
+        /// <param name="min">시작값</param>
+        /// <param name="max">최대값</param>
+        /// <returns>min부터 max까지의 곱</returns>
+        public int Multiply(int min, int max)
+        {
+            int res = 1;
+            for (int i = min; i <= max; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
     }
 }
