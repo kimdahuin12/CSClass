66e9ee7821a463ef1c81179d37e3a58a8af0d511
dahuin <s2001@e-mirim.hs.kr>
2022-06-02 12:12:10 +0900

    #11 소멸자 c#은 가비지컬렉터에서 소멸이 관리해서 어느 시점에 객체가 소멸되는지 명확히 알 수 없다.

--- CSClass/Sample.cs
+++ CSClass/Sample.cs
@@ -19,5 +19,12 @@ namespace CSClass
             value = 11;
             Console.WriteLine("Sample() 정적 생성자 호출!!!");
         }
+
+        //c#은 가비지 컬렉터가 관리를 해서, 소멸자는 언제 소멸될 지 모른다.
+        ~Sample()
+        {
+            Console.WriteLine("Sample() 소멸자 호출!!!!!!!");
+        }
+
     }
 }
